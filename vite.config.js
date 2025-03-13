9df69ecb4eecb76820a4c976f8552bb389b40ee7
Suresh-cs-q <ft24-bscs-0022@qu.edu.pk>
2025-03-13 13:19:33 +0500

    fix: update build configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 3000,
   },
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
   resolve: {
     alias: {
